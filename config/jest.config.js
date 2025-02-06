a99ad4c4b44a1cf6ae2ac25bf8467477c3465ca2
mikrivorot <m.i.krivorot@gmail.com>
2025-02-06 14:40:45 +0100

    change projects structure

---
+++ config/jest.config.js
@@ -0,0 +1,13 @@
+
+const path = require('path');
+
+module.exports = {
+    rootDir: path.resolve(__dirname, '..'), // Adjust rootDir to projects/grpc/typescript_example
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ['<rootDir>/tests/**/*.test.ts'], // Ensure this is relative to rootDir
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageReporters: ['json', 'lcov', 'text', 'clover'],
+};
